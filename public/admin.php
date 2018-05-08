52a162a61245ec402a76e4a7f6fd95c1b0d86071
MiticJovan <miticjovan87@gmail.com>
2018-05-08 12:57:17 +0200

    Changes made new

---
+++ public/admin.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Get values</title>
+	<link rel="stylesheet" media="all" type="text/css" href="css/public.css">
+	<link href="https://fonts.googleapis.com/css?family=Gothic+A1:400,700" rel="stylesheet">
+</head>
+<body>
+<div id="header">
+	<h1>Blog Page</h1>
+</div>
+<div id="main">
+	<div id="navigation">
+		&nbsp;
+	</div>
+	<div id="page">
+		<h2>Admin Menu</h2>
+		<p>Welcome to the admin area</p>
+		<ul>
+			<li><a href="manage_content.php">Manage Website Content</a></li>
+			<li><a href="mannage_admins.php">Manage Admin Users</a></li>
+			<li><a href="logout.php">Log Out</a></li>
+		</ul> 
+	</div>
+</div>
+<div id="footer">Copyright 2018, OMS Zone &nbsp; author Mitic Jovan</div>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Get values</title>
+</head>
+<body>
+
+</body>
+</html>
