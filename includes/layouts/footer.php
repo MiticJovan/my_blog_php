aef6875b89e757651af6cf3a386507bcafea4db1
MiticJovan <miticjovan87@gmail.com>
2018-05-10 12:53:57 +0200

    Fresh changes

---
+++ includes/layouts/footer.php
@@ -0,0 +1,9 @@
+<div id="footer"> Copyright &copy; 2018,&nbsp; OMS Zone &nbsp; author: Mitic Jovan</div>
+</body>
+</html>
+<?php 
+	// 5.Close database connection
+if (isset($connection)) {
+	mysqli_close($connection);
+}
+?> 
---
+++ includes/layouts/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Blog</title>
+	<link rel="stylesheet" media="all" type="text/css" href="public/css/public.css">
+	<link rel="stylesheet" media="all" type="text/css" href="css/public.css">
+	<link href="https://fonts.googleapis.com/css?family=Gothic+A1:400,700" rel="stylesheet">
+</head>
+<body>
+<div id="header">
+	<h1>Blog Page</h1>
+</div>
