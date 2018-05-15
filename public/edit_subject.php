297e0f2499de35283c19ed9888ebd0e0928893ba
MiticJovan <miticjovan87@gmail.com>
2018-05-15 17:26:42 +0200

    changes

---
+++ public/edit_subject.php
@@ -0,0 +1,55 @@
+	<?php require_once("../includes/session.php"); ?> 
+	<?php require_once("../includes/db_connection.php"); ?>
+	<?php require_once("../includes/functions.php"); ?> 
+	<?php require_once("../includes/validation_functions.php"); ?> 
+
+	<?php find_selected_page(); ?>
+
+	<?php if (!$current_subject) {
+		redirect_to("manage_content.php"); 
+	} ?> 
+
+	<?php include("../includes/layouts/header.php"); ?> 
+	
+ 
+
+	<div id="main">
+		<div id="navigation">
+			<?php echo navigation($current_subject, $current_page); ?>
+		</div>
+		<div id="page">
+			<?php echo message(); ?> 
+			<?php $errors = errors(); ?> 
+			<?php echo form_errors($errors); ?>
+			<h2>Edit news : <?php echo $current_subject["manu_name"]; ?></h2>
+			<form action="create_subject.php" method="post">
+			<p>Post name:
+				<input type="text" name="manu_name" value="<?php echo $current_subject["manu_name"]; ?>">
+			</p> 
+			<p>Position:
+			<select name="position">
+				<?php 
+				$subject_set = find_all_subjects();
+				$subject_count = mysqli_num_rows($subject_set);				
+				for ($count = 1; $count <=  $subject_count; $count++){
+					echo "<option value=\"{$count}\"";
+					if ($current_subject["position"] == $count) {
+						echo " selected";
+					} 
+					echo ">{$count}</option>";  
+				}
+				 ?> 
+			</select>
+			</p>
+			<p>Visible:
+				<input type="radio" name="visible" value="0" <?php if ($current_subject["visible"] == 0) { echo "checked"; } ?> > No &nbsp;
+				<input type="radio" name="visible" value="1" <?php if ($current_subject["visible"] == 1) { echo "checked"; } ?> > Yes	
+			</p>
+			<input type="submit" name="submit" value="Edit Subject">
+			</form>
+			<br>
+			<a href="manage_content.php">Cancel</a>
+		</div>
+	</div>
+
+			<?php include("../includes/layouts/footer.php") ?> 
